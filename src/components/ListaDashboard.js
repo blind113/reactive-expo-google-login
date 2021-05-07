2404fec052c7d73861a6b2c4ef74e93c768ba008
Alfredo Osorio <blind.osorio@gmail.com>
2021-05-07 18:44:19 -0300

    Atualização 05/07

--- src/components/ListaDashboard.js
+++ src/components/ListaDashboard.js
@@ -1,14 +1,55 @@
 import React, { Component } from 'react';
-const DATA=[
-    {},{},{},{}
-]
+import { FlatList, View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+const DATA = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'First Item',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'Second Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Third Item',
+    }
+  ];
+const renderItem = ({ item }) => (
+    <Item title={item.title} />
+);
+const Item = ({ title }) => (
+    <View style={styles.item}>
+        <TouchableOpacity>
+            <Text>{title}</Text>
+        </TouchableOpacity>
+    </View>
+  );
+
 class ListaDashboard extends Component{
     render(){
         return(
-            <>
-            </>
+            
+            <FlatList
+                data={DATA}
+                renderItem={renderItem}
+                keyExtractor={item => item.id}
+            />
+            
         )
     }
-}
+};
+const styles = StyleSheet.create({
+    item: {
+      backgroundColor: '#f9c2ff',
+      padding: 20,
+      marginVertical: 8,
+      marginHorizontal: 16,
+    },
+    title: {
+      fontSize: 32,
+    },
+  });
+  
+
 
 export default ListaDashboard;
--- src/screens/DashboardScreen.js
+++ src/screens/DashboardScreen.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
 import {
     View, 
-    Text,
     StyleSheet,
-    Image,
     SafeAreaView,
     ScrollView
 } from "react-native";
 import Menu from '../components/Menu';
 import Footer  from '../components/Footer';
+import ListaDashboard from "../components/ListaDashboard";
 
 class DashboardScreen extends Component{
     
@@ -26,15 +25,7 @@ class DashboardScreen extends Component{
                 </View>
                 <View style={ style.container }>   
                     <ScrollView >
-                        <Text style={style.textContainer}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-
-                        </Text>
+                        <ListaDashboard />
                     </ScrollView>
                 </View>   
                 <View style={style.footerView}>  
